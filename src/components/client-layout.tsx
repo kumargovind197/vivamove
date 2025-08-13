d6037235f65df252478063ad281e3cdfe11c65f7
v Shah <vinitkiranshah@gmail.com>
2025-08-13 19:31:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/client-layout.tsx
@@ -0,0 +1,29 @@
+
+"use client";
+
+import AppFooter from '@/components/app-footer';
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <div className="animated-background">
+        <div className="powder powder-1"></div>
+        <div className="powder powder-2"></div>
+        <div className="powder powder-3"></div>
+        <div className="powder powder-4"></div>
+        <div className="powder powder-5"></div>
+        <div className="powder powder-6"></div>
+      </div>
+      <div className="flex-grow flex flex-col z-10">
+        <main className="flex-grow">
+          {children}
+        </main>
+        <AppFooter />
+      </div>
+    </>
+  );
+}
