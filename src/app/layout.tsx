bff26db2168d817edfdfd994b908aa595689cf62
v Shah <vinitkiranshah@gmail.com>
2025-08-13 08:41:51 +0000

    Ok i can see the colours but hardly noticeable. Maybe add more colours t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
            <div className="powder powder-2"></div>
            <div className="powder powder-3"></div>
            <div className="powder powder-4"></div>
+           <div className="powder powder-5"></div>
+           <div className="powder powder-6"></div>
         </div>
         <div className="flex-grow flex flex-col z-10">
           <main className="flex-grow">
